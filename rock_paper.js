31f5567fb1205a4fe65c801cde83fcbb8e9cbb1c
Matt Shannon <shannon.matt@outlook.com>
2017-09-25 15:04:45 -0600

    initial commit

---
+++ rock_paper.js
@@ -0,0 +1,61 @@
+
+var userChoice = prompt("Do you choose rock, paper or scissors?");
+var computerChoice = Math.random();
+if (computerChoice < 0.34) {
+	computerChoice = "rock";
+} else if(computerChoice >=0.34 && computerChoice <=0.66) {
+	computerChoice = "paper";
+} else {
+	computerChoice = "scissors";
+} console.log("Computer: " + computerChoice);
+
+var compare = function(userChoice, computerChoice) {
+  if(choice1 === choice2) {
+        return "The result is a tie!";
+}
+  else if(choice1 === "rock") {
+    if(choice2 === "scissors") {
+        return "rock wins";
+    }
+    else {
+        return "paper wins";
+    }
+  }
+  else if(choice1 === "paper") {
+    if(choice2 === "rock") {
+        return "paper wins";
+    }
+    else {
+        return "scissors wins";
+    }
+}
+    else if(choice1 === "scissors") {
+      if(choice2 === "rock") {
+       return "rock wins";
+ }
+      else {
+        return "scissors wins";
+    }
+    }
+    else if(choice1 === "scissors") {
+      if(choice2 === "paper") {
+       return "scissors wins";
+}
+      else {
+        return "rock wins";
+    }
+}
+};
+
+
+
+
+/*
+var inputs = document.getElementsByClassName('input');
+for (var i = 0; i < inputs.length; i++) {
+  inputs[i].addEventListener('keyup', function(e) {
+    var tag = e.target.dataset.tag;
+    document.getElementById(tag).innerText = e.target.value;
+  });
+}
+*/
